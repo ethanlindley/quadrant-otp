06c2af3a0060f54f5f66c06fc11c6f94df06f61b
Ethan Lindley <ethanlindley@null.net>
2018-05-18 15:55:15 -0500

    mdhandler: add register and unregister methods

--- server/message_director/MDHandler.py
+++ server/message_director/MDHandler.py
@@ -1,5 +1,6 @@
 from direct.distributed.PyDatagramIterator import PyDatagramIterator
 
+from server.types import MessageTypes as msg_types
 from server.handlers.PacketHandler import PacketHandler
 from lib.logging.Logger import Logger
 
@@ -9,6 +10,9 @@ class MDHandler(PacketHandler):
 
     def __init__(self):
         PacketHandler.__init__(self)
+
+        self.registered_handlers = {}
+
         self.configure()
 
     def configure(self):
@@ -22,3 +26,23 @@ class MDHandler(PacketHandler):
         dgi = PyDatagramIterator(dg)
         msg = dgi.getUint16()
         self.logger.debug("received message - %d" % msg)
+        
+        # begin handling messages here
+        if msg == msg_types.CONTROL_SET_CHANNEL:
+            channel = dgi.getUint16()
+            connection = dg.getConnection()
+            self.register_channel(channel, connection)
+        elif msg == msg_types.CONTROL_REMOVE_CHANNEL:
+            channel = dgi.getUint16()
+            self.unregister_channel(channel)
+        
+
+    def register_channel(self, channel, connection):
+        if self.registered_handlers[channel] is None:
+            self.registered_handlers[channel] = connection
+            self.logger.debug("registered new channel - %d" % channel)
+        
+    def unregister_channel(self, channel):
+        if self.registered_handlers[channel]:
+            del self.registered_handlers[channel]
+            self.logger.debug("unregistered channel - %d" % channel)
